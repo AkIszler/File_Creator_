29a0ed857a3b9df0eb095f0722c9362c88dadde2
Anthony <aiszler29@gmail.com>
2023-04-14 15:39:45 -0700

    switch to desktop

--- gui_app_interface.py
+++ gui_app_interface.py
@@ -6,3 +6,6 @@ frm.grid()
 ttk.Label(frm,text="Hello World").grid(column=0,row=0)
 ttk.Button(frm,text="Click Me").grid(column=1,row=0)
 
+ttk.PanedWindow().grid(column=1,row=2)
+
+root_file.mainloop()

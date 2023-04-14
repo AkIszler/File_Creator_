953457abbc37533b040aec46fe0e27f628f2b706
Anthony <aiszler29@gmail.com>
2023-04-14 15:39:43 -0700

    minor changes / jump to desktop

--- App.py
+++ App.py
@@ -5,9 +5,10 @@ full_file_name = Fsel + "." + Ftype
 
 root_location = input("do you want to use different location? (y/n):")
 root_location_check = root_location.lower()
-if root_location == "y":
+
+if root_location_check == "y":
     root_locations = input("Enter file location: ") 
-elif root_location == "n":
+elif root_location_check == "n":
     print("okay, file will be saved in current directory")
 
 start_line = input("Enter start line: ")

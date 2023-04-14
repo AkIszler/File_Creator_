166e70b11959b01575976af5f77bf144842e02fe
Anthony Iszer <aiszler29@gmail.com>
2023-04-14 11:45:07 -0700

    first commit

---
+++ App.py
@@ -0,0 +1,17 @@
+
+Fsel = input("Enter file name: ")
+Ftype = input("Enter file type: ")
+full_file_name = Fsel + "." + Ftype
+
+root_location = input("do you want to use different location? (y/n):")
+root_location_check = root_location.lower()
+if root_location == "y":
+    root_locations = input("Enter file location: ") 
+elif root_location == "n":
+    print("okay, file will be saved in current directory")
+
+start_line = input("Enter start line: ")
+
+f = open(full_file_name, "x")
+f.write(start_line)
+f.close()
---
+++ gui_app_interface.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+root_file = Tk()
+frm = ttk.Frame(root_file,padding=15)
+frm.grid()
+ttk.Label(frm,text="Hello World").grid(column=0,row=0)
+ttk.Button(frm,text="Click Me").grid(column=1,row=0)
+
